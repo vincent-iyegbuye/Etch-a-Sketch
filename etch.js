b21aef22bfa56ad75a9ab8ff835ef47771aa1488
Vincent Iyegbuye <vincentiyegbuye@gmail.com>
2026-04-22 11:47:33 -0500

    feat: add resizable grid with input validation
    Added a resize button that prompts the user for a new grid size and regenerates the sketchpad based on the entered value.
    The existing grid is cleared before rebuilding, the total drawing area remains the same, and input is restricted to values between 1 and 100 to prevent performance issues.

--- etch.js
+++ etch.js
@@ -1,10 +1,30 @@
 const container = document.querySelector('.container');
-const gridSize = 16;
+let gridSize = 16;
+
+const resizeBtn = document.querySelector("#resizeBtn");
+resizeBtn.textContent = `${gridSize} x ${gridSize}`;
+
+resizeBtn.addEventListener("click", () =>{
+    const userInput = prompt("Enter a grid size from 1 to 100:");
+    const value = parseInt(userInput);
+
+    if(!isNaN(value) && value >=1 && value <= 100){
+        gridSize = value;
+        buildGrid(gridSize);
+        resizeBtn.textContent = `${gridSize} x ${gridSize}`; 
+    } else{
+        console.warn("Enter a number between 1 and 100.");
+    }
+})
 function buildGrid(size){
     container.replaceChildren();
+
+    const squareSize = 960 / size;
     for(let i = 0; i < (size * size); i++){
         const squareDiv = document.createElement('div');
         squareDiv.classList.add('square');
+        squareDiv.style.width = `${squareSize}px`;
+        squareDiv.style.height = `${squareSize}px`;
         container.appendChild(squareDiv);
 
         //const currColor = getComputedStyle(squareDiv).backgroundColor
